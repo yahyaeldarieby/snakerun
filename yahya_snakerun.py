274041bb1c019de17bd4c7ae9b1ccb9eaa503053
yahyaeldarieby <54864418+yahyaeldarieby@users.noreply.github.com>
2020-01-06 17:32:14 -0600

    Update yahya_snakerun.py

--- yahya_snakerun.py
+++ yahya_snakerun.py
@@ -12,16 +12,17 @@
 
 # Forever Loop
 # x = Get user input
-# if(x) = Arrow A then press enter then the snake will move the to left
-# if(x) = Arrow D then press enter then the snake will move the to right
-# if(x) = Arrow W then press enter then the snake will move the to up
-# if(x) = Arrow S then press enter then the snake will move the to down
+# if(x) = Key 4 then press enter then the snake will move left
+# if(x) = Key 3 then press enter then the snake will move right
+# if(x) = Key 1 then press enter then the snake will move up
+# if(x) = Key 2 then press enter then the snake will move down
+# if(x) = Key 5 then press enter to quit the whole system
 
 # If new location has a fruit, then snake-length is doubled (or bigger)
 # If new location has a obstacle, then the snake dies and exit the game
 # If new location has a part of the snake, then the snake dies and exit the game
 # End of forever loop
-# end of program
+# End of program
 
 # The following is a description of the game rules to be displayed at the start of the game
 print ("# Explanation of game")
@@ -29,10 +30,12 @@ print ("The game works by you being a snake and that same snake has to eat fruit
 print ("# How will you survive")
 print ("You will survive the game by avoiding the obstacles and by avoiding hitting yourself.")
 print ("# How to play the game")
-print ("You simply just use the arrow keys to move up, down, right and left.")
+print ("You simply just use the keys 1, 2, 3, 4 to move up, down, right and left.")
 print ("# How will you win the game")
-print ("You can't really win the game but you try to beat your own high score from the last game you had." )
+print ("You can't really win the game but you try to beat your own high score from the last game you played." )
 print ("# How does the game end")
 print ("The game will be over if you hit yourself and if any of the obstacles hit you.")
 print ("# How do you enjoy the game")
 print ("To enjoy the game you have to try to focus on what steps you're taking to beat your high score.")
+print ("# System Quit")
+print ("To quit the whole system press the key 5 then hit enter to quit the whole system.")
