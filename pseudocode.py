3bc41edc3a476105891476c19796f185642a9ad7
yahyaeldarieby <54864418+yahyaeldarieby@users.noreply.github.com>
2020-01-09 17:33:33 -0600

    Update pseudocode.py

--- pseudocode.py
+++ pseudocode.py
@@ -8,34 +8,33 @@
 # Display a welcoming branding image
 # Pick an intial location for snake
 # Pick an initial location for fruits and obstacles
-# Snake length = small
 
 # Forever Loop
 # x = Get user input
-# if(x) = Key 4 then press enter then the snake will move left
-# if(x) = Key 3 then press enter then the snake will move right
-# if(x) = Key 1 then press enter then the snake will move up
-# if(x) = Key 2 then press enter then the snake will move down
-# if(x) = Key 5 then press enter to quit the whole system
+# if(x) = Key A then press enter then the snake will move left
+# if(x) = Key D then press enter then the snake will move right
+# if(x) = Key W then press enter then the snake will move up
+# if(x) = Key S then press enter then the snake will move down
+# if(x) = Key Q then press enter to quit the whole system
+# if(x) = Key H then press enter to restart the whole game
 
-# If new location has a fruit, then snake-length is doubled (or bigger)
-# If new location has a obstacle, then the snake dies and exit the game
-# If new location has a part of the snake, then the snake dies and exit the game
+# If new location has a fruit, then score increases by 10
+# If new location has a obstacle, then the snake dies and the system quits the whole game.
 # End of forever loop
 # End of program
 
 # The following is a description of the game rules to be displayed at the start of the game
 print ("# Explanation of game")
-print ("The game works by you being a snake and that same snake has to eat fruits to get bigger and there will be obstacles in your way to make you lose.")
+print ("The game works by you being a snake and that same snake has to eat fruits to increase your score and there will be obstacles in your way to make you lose.")
 print ("# How will you survive")
-print ("You will survive the game by avoiding the obstacles and by avoiding hitting yourself.")
+print ("You will survive the game by avoiding the obstacles and by avoiding hitting the boarders.")
 print ("# How to play the game")
-print ("You simply just use the keys 1, 2, 3, 4 to move up, down, right and left.")
+print ("You simply just use the keys W, A, S, D to move up, down, right and left.")
 print ("# How will you win the game")
-print ("You can't really win the game but you try to beat your own high score from the last game you played." )
+print ("You will win the game by reaching the GOAL box." )
 print ("# How does the game end")
-print ("The game will be over if you hit yourself and if any of the obstacles hit you.")
+print ("The game will be over if you hit the boarders and if any of the obstacles hit you.")
 print ("# How do you enjoy the game")
-print ("To enjoy the game you have to try to focus on what steps you're taking to beat your high score.")
+print ("To enjoy the game you have to try to focus on what steps you're taking to reach your goal.")
 print ("# System Quit")
-print ("To quit the whole system press the key 5 then hit enter to quit the whole system.")
+print ("To quit the whole system press the key Q then hit enter to quit the whole system.")
